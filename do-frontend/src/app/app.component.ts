ce93d1ea2d0a0ca3aba3e6f62d5364d65fd33e55
Krzysztof Miszke <krzysztofmiszke@Krzysztofs-MacBook-Air.local>
2021-07-16 14:52:22 +0200

    Grid implementation

--- do-frontend/src/app/app.component.ts
+++ do-frontend/src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'do-frontend';
+  title = 'do-app';
+
+  production = environment.production;
+
+  tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
 }
